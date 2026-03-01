c27cc4c55d68414a59e9f19a734adf2a752531c9
NTG259 <truonggiang29122005@gmail.com>
2026-03-02 00:32:14 +0700

    config openvidu

--- BE/build.gradle.kts
+++ BE/build.gradle.kts
@@ -26,9 +26,14 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
+//	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation ("org.springframework.boot:spring-boot-starter")
+	runtimeOnly ("com.mysql:mysql-connector-j")
+	testImplementation ("org.springframework.boot:spring-boot-starter-test")
+	implementation("io.openvidu:openvidu-java-client:2.32.1")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
---
+++ BE/src/main/java/com/meet/BE/controller/SessionController.java
@@ -0,0 +1,45 @@
+package com.meet.BE.controller;
+
+import io.openvidu.java.client.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/sessions")
+public class SessionController {
+
+    private final OpenVidu openVidu;
+
+    public SessionController(OpenVidu openVidu) {
+        this.openVidu = openVidu;
+    }
+
+    @PostMapping("/{room}")
+    public String createSession(@PathVariable String room) throws Exception {
+
+        SessionProperties properties = new SessionProperties.Builder()
+                .customSessionId(room)
+                .build();
+
+        Session session = openVidu.createSession(properties);
+
+        return session.getSessionId();
+    }
+
+    @PostMapping("/{room}/connections")
+    public String createConnection(@PathVariable String room) throws Exception {
+
+        Session session = openVidu.getActiveSession(room);
+
+        if (session == null) {
+            session = openVidu.createSession(
+                    new SessionProperties.Builder()
+                            .customSessionId(room)
+                            .build()
+            );
+        }
+
+        Connection connection = session.createConnection();
+
+        return connection.getToken();
+    }
+}
