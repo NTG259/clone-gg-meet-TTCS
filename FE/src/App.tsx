59321269a6a7eb07e6fe9e0f096407b32a4ae4fa
daubong240705-art <daubong240705@gmail.com>
2026-03-02 23:12:11 +0700

    p

---
+++ FE/src/App.tsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+import { joinRoom } from './test'
+
+function App() {
+
+
+  return (
+    <>
+      <button onClick={() => joinRoom()}>
+
+      </button>
+    </>
+  )
+}
+
+export default App
---
+++ FE/src/test.tsx
@@ -0,0 +1,17 @@
+import { OpenVidu } from "openvidu-browser";
+
+const OV = new OpenVidu(); const session = OV.initSession();
+export async function joinRoom() {
+    const roomName = "test";
+    await fetch(`http://100.90.25.32:8080/api/sessions/${roomName}`,
+        { method: "POST" });
+    const tokenResponse = await fetch(`http://100.90.25.32:8080/api/sessions/${roomName}/connections`,
+        { method: "POST" });
+    const token = await tokenResponse.text();
+    console.log(token);
+    await session.connect(token);
+
+    const publisher = await OV.initPublisherAsync(undefined, { audioSource: undefined, videoSource: undefined, publishAudio: true, publishVideo: true });
+    session.publish(publisher);
+    console.log("Connected successfully");
+}
